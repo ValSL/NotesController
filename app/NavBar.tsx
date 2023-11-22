d5a3412a1e0241e1dd09cc25b5b57c80f0aa1225
ValSL <valid.saley@gmail.com>
2023-11-22 21:12:15 +0300

    Styling active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,24 +1,42 @@
+'use client'
+
 import Link from "next/link";
-import React from 'react'
+import React from "react";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
-  const links = [
-    {label: 'Dashboard', href: '/'},
-    {label: 'Issues', href: '/issues'},
-  ]
-  
-    return (
-    <nav className="flex gap-6 border-b px-5 h-14 mb-5 items-center">
-        <Link href='/'>Logo</Link>
-        <ul className="flex gap-6">
-            {links.map((item) => {
-                return (
-                    <Link key={item.href} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={item.href}>{item.label}</Link>
-                )
-            })}
-        </ul>
-    </nav>
-  )
-}
+    const currentPath = usePathname();
 
-export default NavBar
+	const links = [
+		{ label: "Dashboard", href: "/" },
+		{ label: "Notes", href: "/notes" },
+	];
+
+	return (
+		<nav className="flex gap-6 border-b px-5 h-14 mb-5 items-center">
+			<Link href="/">Logo</Link>
+			<ul className="flex gap-6">
+				{links.map((item) => {
+					return (
+						<Link
+							key={item.href}
+							className={
+                                classNames({
+                                    'text-zinc-900': currentPath === item.href,
+                                    'text-zinc-500': currentPath !== item.href,
+                                    'hover:text-zinc-800 transition-colors': true
+                                })
+                            }
+							href={item.href}
+						>
+							{item.label}
+						</Link>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;
