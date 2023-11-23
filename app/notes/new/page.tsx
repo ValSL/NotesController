44ed7d059b006bc339292107d47f769eaa120640
ValSL <valid.saley@gmail.com>
2023-11-23 19:37:11 +0300

    add MDE editor

--- app/notes/new/page.tsx
+++ app/notes/new/page.tsx
@@ -1,12 +1,14 @@
+'use client'
+
 import { Button, TextInput, Textarea } from "@mantine/core";
-// import { TextField } from "@radix-ui/themes";
-import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewNotePage = () => {
   return (
     <div className="max-w-xl space-y-3">
         <TextInput placeholder="Title"/>
-        <Textarea placeholder="Description"/>
+        <SimpleMDE placeholder="Description"/>
         <Button>Submit new Note</Button>
     </div>
   )
