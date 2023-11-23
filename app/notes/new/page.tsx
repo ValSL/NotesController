6957dd18ce3df71b00300a467aae788a94d343ea
ValSL <valid.saley@gmail.com>
2023-11-23 22:18:42 +0300

    simple creation new notes

--- app/notes/new/page.tsx
+++ app/notes/new/page.tsx
@@ -1,17 +1,37 @@
-'use client'
+"use client";
 
 import { Button, TextInput, Textarea } from "@mantine/core";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-const NewNotePage = () => {
-  return (
-    <div className="max-w-xl space-y-3">
-        <TextInput placeholder="Title"/>
-        <SimpleMDE placeholder="Description"/>
-        <Button>Submit new Note</Button>
-    </div>
-  )
+interface NewNoteForm {
+	title: string;
+	description: string;
 }
 
-export default NewNotePage
+const NewNotePage = () => {
+	const { register, handleSubmit, control } = useForm<NewNoteForm>();
+	const router = useRouter();
+
+	const submitHandler: SubmitHandler<NewNoteForm> = async (data) => {
+		await axios.post("/api/notes", data);
+		router.push("/");
+	};
+
+	return (
+		<form className="max-w-xl space-y-3" onSubmit={handleSubmit(submitHandler)}>
+			<TextInput placeholder="Title" {...register("title")} />
+			<Controller
+				control={control}
+				name="description"
+				render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+			/>
+			<Button type="submit">Go</Button>
+		</form>
+	);
+};
+
+export default NewNotePage;
